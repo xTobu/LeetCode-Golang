d153d98de2ddc490c949efef68bbdb9af6d1e56e
junxiang <junxiang.oriint@gmail.com>
2019-07-15 16:41:54 +0800

    [Update]  Colorize `go test` output
    with 'github.com/kyoh86/richgo'

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/xTobu/LeetCode-Golang
 
 go 1.12
 
-require github.com/stretchr/testify v1.3.0
+require (
+	github.com/kyoh86/richgo v0.3.1 // indirect
+	github.com/stretchr/testify v1.3.0
+)
--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
-echo "" > coverage.txt
+# echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
     echo $d
-    go test -coverprofile=coverage.out -covermode=atomic $d
+    richgo test -coverprofile=coverage.out -covermode=atomic $d
     # if [ -f coverage.out ]; then
     #     # cat coverage.out >> coverage.txt
     #     # rm coverage.out
