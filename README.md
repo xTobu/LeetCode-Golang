e9ceed3dd3bcd15c79440d917251c6537d7ce885
junxiang <junxiang.oriint@gmail.com>
2019-07-15 16:05:49 +0800

    [update] 增加測試配置

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # LeetCode-Golang
 LeetCode with Golang
+
+---
+
+## Testing
+
+### Run Test
+
+```bash
+bash ./test.bash
+```
+
+### Display details
+```bash
+# Function
+go tool cover -func=coverage.out
+
+# HTML
+go tool cover -html=coverage.out
+```
---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v vendor); do
+    echo $d
+    go test -coverprofile=coverage.out -covermode=atomic $d
+    # if [ -f coverage.out ]; then
+    #     # cat coverage.out >> coverage.txt
+    #     # rm coverage.out
+    # fi
+done
