2b2d5b11c6c414a2f66fed9cc8ca6d4ae1ecf862
junxiang <junxiang.oriint@gmail.com>
2019-09-05 17:12:55 +0800

    [Update] 206. 清除多餘程式碼，該段已移至 utils/ListNode

--- algorithms/0206.Reverse-Linked-List/reverse-linked-list.go
+++ algorithms/0206.Reverse-Linked-List/reverse-linked-list.go
@@ -70,35 +70,3 @@ func reverseList(head *ListNode) *ListNode {
  * 以 prev 置換 Next 的方式堆疊起來
  *
  */
-
-// convert *ListNode to []int
-func l2s(head *ListNode) []int {
-	res := []int{}
-
-	for head != nil {
-		res = append(res, head.Val)
-		head = head.Next
-	}
-
-	return res
-}
-
-// convert []int to *ListNode
-func s2l(nums []int) *ListNode {
-	if len(nums) == 0 {
-		return nil
-	}
-
-	res := &ListNode{
-		Val: nums[0],
-	}
-	temp := res
-	for i := 1; i < len(nums); i++ {
-		temp.Next = &ListNode{
-			Val: nums[i],
-		}
-		temp = temp.Next
-	}
-
-	return res
-}
